6eb0db681cb72b93a57aa69393b317ee5bc50f06
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-20 18:24:03 +0300

    feat(room-form): create a form html with createRoom model; display created Room in home page.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
-
+from .forms import RoomForm
 # rooms = [
 #     { 'id':1 , 'name': 'Lets learn python' },
 #     { 'id':2 , 'name': 'Design with me' },
@@ -17,3 +17,12 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
