8826b716212e1ae02ecd1cf79323032559e9ae4a
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-23 16:01:47 +0300

    feat: create updateRoom model for update existing room

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,7 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Room
 
-class RoomForm(forms.ModelForm):
+class RoomForm(ModelForm):
     class Meta():
         model =  Room
         fields =  '__all__'
--- base/models.py
+++ base/models.py
@@ -14,12 +14,15 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants = 
-    updated = models.DateTimeField(auto_created=True)
-    create = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__ (self):
         return self.name
-   
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -29,4 +32,3 @@ class Message(models.Model):
 
     def __str__ (self):
         return self.body[0:50]
-    
